b703ad8a63a9a3001ee1bdd66d92f114a829d7f4
Frank Lemanschik <frank@lemanschik.com>
2022-03-21 21:38:32 +0100

    Update index.d.ts
    The typings for the new api while dropped the typings for the old one

--- index.d.ts
+++ index.d.ts
@@ -1,84 +1,61 @@
-import {GlobbyOptions} from 'globby';
-
-declare namespace del {
-	interface Options extends GlobbyOptions {
-		/**
-		Allow deleting the current working directory and outside.
-
-		@default false
-		*/
-		readonly force?: boolean;
-
-		/**
-		See what would be deleted.
-
-		@default false
-
-		@example
-		```
-		import del = require('del');
-
-		(async () => {
-			const deletedPaths = await del(['temp/*.js'], {dryRun: true});
-
-			console.log('Files and directories that would be deleted:\n', deletedPaths.join('\n'));
-		})();
-		```
-		*/
-		readonly dryRun?: boolean;
-
-		/**
-		Concurrency limit. Minimum: `1`.
-
-		@default Infinity
-		*/
-		readonly concurrency?: number;
-	}
+// v13.1.1 export type GlobbyOptions = import('globby').Options;
+export type GlobbyOptions = import('globby').GlobbyOptions;
+export interface delOptionPropertys {
+	/**
+	 * default false
+	 */
+	force?: boolean;
+	/**
+	 * default false
+	 */
+	dryRun?: boolean;
+	/**
+	 * default false
+	 */
+	cwd?: string;
 }
+export type delOptions = delOptionPropertys & GlobbyOptions;
+export type patterns = string | readonly string[];
 
-declare const del: {
-	/**
-	Synchronously delete files and directories using glob patterns.
-
-	Note that glob patterns can only contain forward-slashes, not backward-slashes. Windows file paths can use backward-slashes as long as the path does not contain any glob-like characters, otherwise use `path.posix.join()` instead of `path.join()`.
-
-	@param patterns - See the supported [glob patterns](https://github.com/sindresorhus/globby#globbing-patterns).
-	- [Pattern examples with expected matches](https://github.com/sindresorhus/multimatch/blob/main/test/test.js)
-	- [Quick globbing pattern overview](https://github.com/sindresorhus/multimatch#globbing-patterns)
-	@param options - You can specify any of the [`globby` options](https://github.com/sindresorhus/globby#options) in addition to the `del` options. In contrast to the `globby` defaults, `expandDirectories`, `onlyFiles`, and `followSymbolicLinks` are `false` by default.
-	@returns The deleted paths.
-	*/
-	sync: (
-		patterns: string | readonly string[],
-		options?: del.Options
-	) => string[];
-
-	/**
-	Delete files and directories using glob patterns.
-
-	Note that glob patterns can only contain forward-slashes, not backward-slashes. Windows file paths can use backward-slashes as long as the path does not contain any glob-like characters, otherwise use `path.posix.join()` instead of `path.join()`.
-
-	@param patterns - See the supported [glob patterns](https://github.com/sindresorhus/globby#globbing-patterns).
-	- [Pattern examples with expected matches](https://github.com/sindresorhus/multimatch/blob/main/test/test.js)
-	- [Quick globbing pattern overview](https://github.com/sindresorhus/multimatch#globbing-patterns)
-	@param options - You can specify any of the [`globby` options](https://github.com/sindresorhus/globby#options) in addition to the `del` options. In contrast to the `globby` defaults, `expandDirectories`, `onlyFiles`, and `followSymbolicLinks` are `false` by default.
-	@returns The deleted paths.
-
-	@example
-	```
-	import del = require('del');
-
-	(async () => {
-		const deletedPaths = await del(['temp/*.js', '!temp/unicorn.js']);
-
-		console.log('Deleted files and directories:\n', deletedPaths.join('\n'));
-	})();
-	```
-	*/
-	(
-		patterns: string | readonly string[],
-		options?: del.Options
-	): Promise<string[]>;
-};
-
-export = del;
+/**
+ * Synchronously delete files and directories using glob patterns.
+ * Note that glob patterns can only contain forward-slashes, not
+ * backward-slashes. Windows file paths can use backward-slashes
+ * as long as the path does not contain any glob-like characters,
+ * otherwise use `path.posix.join()` instead of `path.join()`.
+ * @param {patterns} patterns - See the supported [glob patterns](https://github.com/sindresorhus/globby#globbing-patterns).
+ *	- [Pattern examples with expected matches](https://github.com/sindresorhus/multimatch/blob/main/test/test.js)
+ *	- [Quick globbing pattern overview](https://github.com/sindresorhus/multimatch#globbing-patterns)
+ * @param {delOptions} delOptions - You can specify any of the
+ * [`globby` options](https://github.com/sindresorhus/globby#options) in addition to the `delOptions`. In contrast to the `globby` defaults, `expandDirectories`, `onlyFiles`, and `followSymbolicLinks` are `false` by default.
+ * @returns {string[]} The deleted paths.
+ * @example
+ * ```
+ * import { delSync } from 'del';
+ * console.log('Deleted files and directories:\n', delSync(['temp/*.js', '!temp/unicorn.js']).join('\n'));
+ * ```
+ */
+export function delSync(patterns: patterns, delOptions: delOptions): string[];
+/**
+ * delete files and directories using glob patterns.
+ * Note that glob patterns can only contain forward-slashes, not
+ * backward-slashes. Windows file paths can use backward-slashes
+ * as long as the path does not contain any glob-like characters,
+ * otherwise use `path.posix.join()` instead of `path.join()`.
+ * @param {patterns} patterns - See the supported [glob patterns](https://github.com/sindresorhus/globby#globbing-patterns).
+ *	- [Pattern examples with expected matches](https://github.com/sindresorhus/multimatch/blob/main/test/test.js)
+ *	- [Quick globbing pattern overview](https://github.com/sindresorhus/multimatch#globbing-patterns)
+ * @param {delOptions} delOptions - You can specify any of the
+ * [`globby` options](https://github.com/sindresorhus/globby#options) in addition to the `delOptions`. In contrast to the `globby` defaults, `expandDirectories`, `onlyFiles`, and `followSymbolicLinks` are `false` by default.
+ * @returns {Promise<string[]>} The deleted paths.
+ * @example
+ * ```
+ * import { delAsync } from 'del';
+ * const deleteAndLogPathes = async (pathesToDelete) => {
+ *	const deletedPaths = await del(pathesToDelete);
+ *	console.log('Deleted files and directories:\n', deletedPaths.join('\n'));
+ * };
+ * deleteAndLogPathes(['temp/*.js', '!temp/unicorn.js']);
+ * ```
+ */
+export function delAsync(patterns: patterns, delOptions: delOptions): Promise<string[]>;
